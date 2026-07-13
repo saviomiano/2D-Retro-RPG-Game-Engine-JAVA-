ab5c5c7f3602de606a485349c92ba2b7b88ed3eb
Savio Miano <146247300+saviomiano@users.noreply.github.com>
2026-07-13 17:21:20 +0100

    Map Layout for milestone 1

--- src/dungeon.java
+++ src/dungeon.java
@@ -1,3 +1,19 @@
-public class dungeon {
-    
+// Dungeon Map
+// author: Sam
+// 10/05/26
+
+//E stands for Enemy
+//B stands for Boss
+//simple map
+
+public class Dungeon{
+
+	static DungeonMap dungeon = new DungeonMap();
+	static Player player = new Player();
+
+	public static void main(String args[]){
+		dungeon.PrintMap();
+		player.Direction();
+		dungeon.Update(player,dungeon);
+	}
 }
